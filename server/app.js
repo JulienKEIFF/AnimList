f7271706c0991e671e12fbdfe9b0b5fe5ab03cf0
julien <j.keiff@hotmail.fr>
2020-05-12 11:49:02 +0200

    fix(server):Add redirection for front

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.sendFile(path.resolve(__dirname, 'public' ,'index.html'))
 });
 
 // error handler
