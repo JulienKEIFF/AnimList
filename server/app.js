035579a7cde7066d30e251575707fa5f651a2ef1
JulienKEIFF <j.keiff@hotmail.fr>
2020-05-15 20:34:32 +0200

    remove cookie parser

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var cors = require('cors')
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -17,7 +16,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
