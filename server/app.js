e9450ac17f79651c6931c3faeda5acea3efc50b5
julien <j.keiff@hotmail.fr>
2020-05-18 17:23:14 +0200

    change cors

--- server/app.js
+++ server/app.js
@@ -8,8 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-app.use(cors())
-app.options('*', cors())
+app.use(cors({
+  origin: 'http://localhost:1235'
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
